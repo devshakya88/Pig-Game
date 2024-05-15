3bdebb2926c073094e64c963ec300389dc001a1f
Devender Shakya <135866940+devshakya88@users.noreply.github.com>
2024-05-16 00:08:06 +0530

    Roll Function Done

---
+++ script.js
@@ -0,0 +1,39 @@
+'use strict';
+
+//Selecting Elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+//Starting Conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+let currentScore = 0;
+
+//Rolling Dice Functionality
+
+btnRoll.addEventListener('click', function () {
+  console.log('Button Click');
+  //1. Generating a Random Dice Roll
+
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  //2. Display Dice
+
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  //3. Check for rolled 1: if true switch to next Player
+  if (dice !== 1) {
+    //Add dice to Current Score
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  } else {
+  }
+});
